9d2944f585e09ba01876f5307da71e3003bb28e5
moreal <dev.moreal@gmail.com>
2019-03-20 22:12:25 +0900

    Update hexedits.decorators to add condition forgotten

--- hexedit/decorators.py
+++ hexedit/decorators.py
@@ -1,7 +1,7 @@
 
 def check_opened(func, *args):
     def new_func(*args):
-        if args[0]._file is not None:
+        if args[0]._file is not None and not args[0]._file.closed:
             return func(*args)
         else:
             raise Exception()
