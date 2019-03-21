1578ed3d6d6d2df10aba6bd388b40f0a4e4cdd46
moreal <dev.moreal@gmail.com>
2019-03-21 09:50:27 +0900

    Add __str__ and change byteorder big -> little to PartitionTableEntry

--- hexedit/structures/partition_table_entry.py
+++ hexedit/structures/partition_table_entry.py
@@ -11,9 +11,12 @@ class PartitionTableEntry:
         self.TOTAL_SECTORS = TOTAL_SECTORS
 
     @staticmethod
-    def from_bytes(raw: bytes) -> PartitionTableEntry:
+    def from_bytes(raw: bytes):
         def to_int(raw: bytes) -> int:
-            return int.from_bytes(raw, byteorder='big')
+            return int.from_bytes(raw, byteorder='little')
 
         return PartitionTableEntry(raw[0], to_int(raw[1:4]), raw[4], to_int(raw[5:8]), to_int(raw[8:12]),
                                    to_int(raw[12:16]))
+
+    def __str__(self):
+        return "Partition Type {:02X} / LBA Address {:08X}".format(self.PARTITION_TYPE, self.START_LBA_ADDRESS)
