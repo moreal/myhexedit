7395ae524fb6a358ef2cca699bdf6f506e06f49c
moreal <dev.moreal@gmail.com>
2019-03-21 10:04:21 +0900

    Remove useless argument from hexedit.decorators

--- hexedit/decorators.py
+++ hexedit/decorators.py
@@ -1,8 +1,8 @@
 
-def check_opened(func, *args):
+def check_opened(func):
     def new_func(*args):
-        if args[0]._file is not None and not args[0]._file.closed:
+        if args[0].get_file() is not None and not args[0].get_file().closed:
             return func(*args)
         else:
-            raise Exception()
+            raise FileNotFoundError()
     return new_func
